41bd552329c8d2012a33dbfe15eb55253f199d42
mrskme <eiriksb@getacademy.no>
2020-11-16 11:15:09 +0100

    asdasd
    asdasd

---
+++ SimpleChess/Rook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+    class Rook : Piece
+    {
+        public Rook() : base("Rook")
+        {
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+    }
+}
